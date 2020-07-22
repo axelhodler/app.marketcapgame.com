b52feca07f0cb13abf3cd31c71212a0c32953a78
Axel Hodler <axel@hodler.co>
2020-07-22 20:38:29 +0200

    initial commit

---
+++ precache-manifest.42bd9d829e727b254d1c824ce56a4afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7e806ca9a6c4619969",
+    "url": "/css/app.06fa51f1.css"
+  },
+  {
+    "revision": "75198918d6a41c0fd129",
+    "url": "/css/chunk-vendors.ecbd622c.css"
+  },
+  {
+    "revision": "65f9c9e8c679ab3f56e7e4da20ae88fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b7e806ca9a6c4619969",
+    "url": "/js/app.508a2e00.js"
+  },
+  {
+    "revision": "75198918d6a41c0fd129",
+    "url": "/js/chunk-vendors.7db3a68d.js"
+  },
+  {
+    "revision": "949a864544d52d70c18a160b0aadf383",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
